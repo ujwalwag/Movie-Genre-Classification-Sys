c3566fc689f68823d60bf80575e50c2c76a16f04
ujwalwag <157630501+ujwalwag@users.noreply.github.com>
2025-05-06 23:22:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ def predict_image():
 
 # ─────────────────────────── Main ───────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
